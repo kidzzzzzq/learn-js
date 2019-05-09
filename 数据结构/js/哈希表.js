e301c9d71bcba8678c631cf1d3138bf3f9908cc0
kid <1519567680@qq.com>
2019-05-09 20:49:55 +0800

    5.9 3

--- 数据结构/js/哈希表.js
+++ 数据结构/js/哈希表.js
@@ -61,10 +61,13 @@ function HashTable() {
 		}
 		//插入操作
 		bucket.push([key,value]);
-
 		this.count++;
+		//判断是否需要扩容
+		if (this.count / this.limit > 0.75) {
+			this.resize(this.limit * 2);
+		}
 	}
-	//获取操作
+	//3.获取操作
 	HashTable.prototype.get = function (key) {
 		// body...
 		//通过key获取index
@@ -86,7 +89,7 @@ function HashTable() {
 		//如果没有key
 		return null;
 	}
-	//删除操作
+	//4.删除操作
 	HashTable.prototype.remove = function (key) {
 		// body...
 		//根据key获取index
@@ -105,37 +108,65 @@ function HashTable() {
 				this.count--;
 				return tuple[1];
 			}
+			//缩小容量
+			if (this.limit>7 && this.count/this.limit<0.25) {
+				this.resize(Math.floor(this.limit/2));
+			}
 		}
 		//没有key
 		return null;
 	}
-	//判断哈希表是否为空
+	//5.判断哈希表是否为空
 	HashTable.prototype.isEmpty = function () {
 		// body...
 		return this.count == 0;
 	}
-	//获取哈希表中元素的个数
+	//6.获取哈希表中元素的个数
 	HashTable.prototype.size = function () {
 		// body...
 		return this.count; 
+	}
+	//7.哈希表扩容
+	HashTable.prototype.resize = function (newLimit) {
+		// body...
+		//保存旧的数组内容
+		var oldStorage = this.storage;
+		//重置所有属性
+		this.storage = [];
+		this.limit = newLimit;
+		this.count = 0;
+		//遍历oldStorage中所有bucket
+		for (var i = 0; i < oldStorage.length; i++) {
+			//取出对应的bucket
+			var bucket = oldStorage[i];
+			//bucket若为空，则执行下一次循环
+			if (bucket == null) {
+				continue;
+			}
+			//bucket若不为空，则取出其中的每组数据放入新表中
+			for (var j = 0; j < bucket.length; j++) {
+				var tuple = bucket.[j];
+				this.put(tuple[0],tuple[1]);
+			}
+		}
 	}
 }
 
 // 哈希表
 // 创建哈希表
 // var hashTable = new HashTable();
-// 用哈希函数得出元素存放在数组中的位置
+// 1.用哈希函数得出元素存放在数组中的位置
 // (此时元素还未添加，数组为空)
 // console.log(hashTable.hashFunc('小黑',11)); //10
 // console.log(hashTable.hashFunc('苍牙',11)); //6
 // console.log(hashTable.hashFunc('琳',11));   //3
 // console.log(hashTable.hashFunc('小椒',11)); //1
 // console.log(hashTable.hashFunc('阿力',11)); //0
-// console.log(hashTable.hashFunc('隼白',23)); //4
-// console.log(hashTable.hashFunc('阴阳师',30)); //7
-// console.log(hashTable.hashFunc('雷神',50)); //9
-// console.log(hashTable.hashFunc('老板娘',24)); //20
-// 添加操作
+// console.log(hashTable.hashFunc('隼白',11)); //6
+// console.log(hashTable.hashFunc('阴阳师',11)); //8
+// console.log(hashTable.hashFunc('雷神',11)); //0
+// console.log(hashTable.hashFunc('老板娘',11)); //3
+// 2.添加操作
 // hashTable.put('小黑',18); 
 // hashTable.put('苍牙',21);
 // hashTable.put('琳',20);
@@ -145,12 +176,12 @@ function HashTable() {
 // hashTable.put('阴阳师',30);
 // hashTable.put('雷神',50);
 // hashTable.put('老板娘',24);
-// 获取操作
+// 3.获取操作
 // console.log(hashTable.get('隼白'));
-// 修改操作
+// 4.修改操作
 // hashTable.put('隼白',25);
 // console.log(hashTable.get('隼白'));
-// 删除操作
+// 5.删除操作
 // hashTable.remove('隼白');
 // console.log(hashTable.get('隼白'));		
 // console.log(hashTable);
