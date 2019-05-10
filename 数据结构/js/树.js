497782a634162f11c74a9a2e2be96097d25a3912
kid <1519567680@qq.com>
2019-05-10 10:53:02 +0800

    5.10 2

---
+++ 数据结构/js/树.js
@@ -0,0 +1,14 @@
+//创建BST
+function BinarySearchTree() {
+	// body...
+	//创建节点构造函数
+	function Node(key) {
+		// body...
+		this.key = key;
+		this.left = null;
+		this.right = null;
+	}
+	//保存根的属性
+	this.root = null;	
+	//二叉搜索树相关的操作方法
+}
