24106b9f699176d3cab1365621eb0aace74a3b43
kid <1519567680@qq.com>
2019-05-20 19:49:43 +0800

    5.20.2

--- 数据结构/js/优先级队列.js
+++ 数据结构/js/优先级队列.js
@@ -11,8 +11,48 @@ function PriorityQueue() {
 	//增
 	PriorityQueue.prototype.enqueue = function(element,priority) {
 		// body...
-		var element = new QueueElement(element,priority);
-		
-	};
+		var queueElement = new QueueElement(element,priority);
+		//队列为空
+		if (this.isEmpty()) {
+			this.items.push(queueElement);
+		} else {
+			var added = false;
+			for (var i = 0; i < this.items.length; i++) {
+				if (queueElement.priority < this.items[i].priority) {
+					this.items.splice(i,0,queueElement);
+					added = true;
+					break;
+				}
+			}
+			if (!added) {
+				this.items.push(queueElement);
+			}			
+		}
+	}
+	//2.删
+	PriorityQueue.prototype.dequeue = function () {
+		// body...
+		return this.items.shift();
+	}
+	//3.查看前端的元素
+	PriorityQueue.prototype.front = function(){
+		return this.items[0];
+	}
+	//4.查看队列是否为空
+	PriorityQueue.prototype.isEmpty = function (){
+		return this.items.length == 0;
+	}
+	//5.查看队列中元素的个数
+	PriorityQueue.prototype.size = function(){
+		return this.items.length;
+	}
+	//6.toString
+	PriorityQueue.prototype.toString = function (){
+		var resultString = '';
+		for (var i = 0; i < this.items.length; i++) {
+			resultString += this.items[i].element + '-' + this.items[i].priority + ' ';
+		}
+		return resultString;
+	}
 }
 //var pq = new PriorityQueue();
